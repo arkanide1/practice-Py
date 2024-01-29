879e984fd102ca6c6f404fa6332224702cc8ee59
Mohamed <mohamedouaquad@gmail.com>
2024-01-29 20:35:46 +0100

    first module

---
+++ modules.py/main.py
@@ -0,0 +1,16 @@
+import utility
+# import outils.more_outils.item
+from outils.more_outils.item import buy
+from utility import multiply, divide
+
+
+from outils.more_outils import * # this is another way of doing it but its not recommended 
+
+# print(utility.multiply(3,4))
+
+# print(outils.more_outils.item.buy("course"))
+
+print(multiply(3, 4))
+
+
+print(buy("course"))
