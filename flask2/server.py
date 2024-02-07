0661a4f107d46565a72a47c57e02aaf729c964fe
Mohamed <mohamedouaquad@gmail.com>
2024-02-07 11:54:42 +0100

    adding a try block to the submit form

--- flask2/server.py
+++ flask2/server.py
@@ -1,44 +1,54 @@
-from flask import Flask , render_template , request , redirect
+from flask import Flask, render_template, request, redirect
 import csv
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        write_to_csv(data)
-        return redirect("/thankyou.html")
-    else :
-        return  "oops something went wrong"
-    
+        try:
+            data = request.form.to_dict()
+            # write_to_file(data)
+            write_to_csv(data)
+            return redirect("/thankyou.html")
+        except:
+            return "fif not save to database"
+    else:
+        return "oops something went wrong"
+
+
 def write_to_file(data):
     with open("data.txt", "a") as database:
         email = data["email"]
-        name =data["name"]
-        message=data["message"]
+        name = data["name"]
+        message = data["message"]
         database.write(f"name:{name} , email:{email} , message:{message}\n")
+
 
 def write_to_csv(data):
     with open("data.txt", "a") as database:
         email = data["email"]
-        name =data["name"]
-        message=data["message"]
+        name = data["name"]
+        message = data["message"]
         database.write(f"name:{name} , email:{email} , message:{message}\n")
 
+
 def write_to_csv(data):
-    with open("data.csv",newline='', mode="a") as database2:
+    with open("data.csv", newline='', mode="a") as database2:
         email = data["email"]
-        name =data["name"]
-        message=data["message"]
-        
-        csv_writer = csv.writer(database2 , delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([name,email,message])
+        name = data["name"]
+        message = data["message"]
+
+        csv_writer = csv.writer(database2, delimiter=",",
+                                quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
